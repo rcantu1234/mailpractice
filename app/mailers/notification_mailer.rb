fb4c2f83757c9b46a9bd5db6826bb819c0998606
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-23 14:45:07 -0600

    Added APIKey

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,13 +1,5 @@
 class NotificationMailer < ApplicationMailer
 
-default from: 'notifications@example.com'
-
-def welcome_email(user)
-  @user = user
-  @url = 'http://roel.cantu@yahoo.com/login'
-  mail(to: @user.email, subject: 'Welcome ty my Awesome Site')
-end
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -16,7 +8,7 @@ end
   def new_user
     @greeting = "Hi"
 
-    mail to: "roel.cantu@yahoo.com"
+    mail to: "to@example.org"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -27,7 +19,7 @@ end
   def new_post
     @greeting = "Hi"
 
-    mail to: "roel.cantu@yahoo.com"
+    mail to: "to@example.org"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
